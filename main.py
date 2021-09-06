d1f4aad8533638437ff3e5e79aa864d0a427683e
Mph96 <matthew.hernandez@hcl.com>
2021-09-05 20:21:38 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+           'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B',
+           'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
+
+password_length = nr_letters + nr_symbols + nr_numbers
+
+letter = 1
+number = 2
+symbol = 3
+i = 0
+password = ""
+
+for i in range(1, password_length):
+    character = random.randint(1, 3)
+    if character == 1:
+        password = password + letters[random.randint(0, len(letters) - 1)]
+    elif character == 2:
+        password = password + numbers[random.randint(0, len(numbers) - 1)]
+    elif character == 3:
+        password = password + symbols[random.randint(0, len(symbols) - 1)]
+
+    i += 1
+print(f"Your generated password is {password}")
